4a93d0a2143eded7cc1a3d16be70f9a0fb305ce2
v.esionov <v.esionov@fstr.tech>
2024-05-29 19:41:12 +0400

    add flex section (50% in progress)

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 const flowSection = document.querySelector(".flow-section");
 const sizeSection = document.querySelector(".size-section");
 const positionSection = document.querySelector(".position-section");
+const flexSection = document.querySelector(".flex-section");
 const navs = document.querySelectorAll(".nav");
 navs.forEach(nav => {
   nav.addEventListener('click', (e) => {
@@ -12,6 +13,8 @@ navs.forEach(nav => {
       sizeSection.scrollIntoView({ block: "start", behavior: "smooth" })
     } else if (classes.contains('position-button')) {
       positionSection.scrollIntoView({ block: "start", behavior: "smooth" })
+    } else if (classes.contains('flex-button')) {
+      flexSection.scrollIntoView({ block: "start", behavior: "smooth" })
     }
   })
 });
@@ -53,75 +56,49 @@ const padding = document.querySelector('#padding'),
       paddingLabel = document.querySelector('#demo-padding-label'),
       paddingBox = document.querySelector('.padding-box'),
       paddingBoxSpan = document.querySelector('.padding-box > span'),
-
       border = document.querySelector('#border'),
       borderLabel = document.querySelector('#demo-border-label'),
       borderBox = document.querySelector('.border-box'),
       borderBoxSpan = document.querySelector('.border-box > span'),
-
       margin = document.querySelector('#margin'),
       marginLabel = document.querySelector('#demo-margin-label'),
       marginBox = document.querySelector('.margin-box'),
       marginBoxSpan = document.querySelector('.margin-box > span'),
-      
       width = document.querySelector('#width'),
       widthLabel = document.querySelector('#demo-width-label'),
-
       height = document.querySelector('#height'),
       heightLabel = document.querySelector('#demo-height-label'),
-
       baseBox = document.querySelector('.base-box')
       sizingSize = document.querySelector('.sizing-size'),
       occupiedSize = document.querySelector('.occupied-size');
 
 const changeSize = (name, event, input, label, span, box) => {
     const value = event.target.value;
-
     if (value < 0) {
       event.preventDefault();
       input.value = 0;
-      return false
+      return false;
     };
-
     if (name === 'width' || name === 'height') {
       box.style[name] = `${value}px`;
     } else {
       span.textContent = `${name}: ${value}px`
       box.style.padding = `${value}px`;
     }
-
     label.textContent = `${name}: ${value}px`
-
     const widthSize = padding.value * 2 + border.value * 2 + baseBox.offsetWidth;
     const heightSize = padding.value * 2 + border.value * 2 + baseBox.offsetHeight;
-
     const widthOccupied = margin.value * 2 + widthSize;
     const heightOccupied = margin.value * 2 + heightSize;
-
     sizingSize.textContent = `${widthSize}px x ${heightSize}px`;
     occupiedSize.textContent = `${widthOccupied}px x ${heightOccupied}px`;
 }
 
-padding.addEventListener(
-    'input', 
-    (event) => changeSize('padding', event, padding, paddingLabel, paddingBoxSpan, paddingBox)
-);
-border.addEventListener(
-    'input', 
-    (event) => changeSize('border', event, border, borderLabel, borderBoxSpan, borderBox)
-);
-margin.addEventListener(
-    'input', 
-    (event) => changeSize('margin', event, margin, marginLabel, marginBoxSpan, marginBox)
-);
-width.addEventListener(
-    'input', 
-    (event) => changeSize('width', event, width, widthLabel, null, baseBox)
-);
-height.addEventListener(
-    'input', 
-    (event) => changeSize('height', event, height, heightLabel, null, baseBox)
-);
+padding.addEventListener('input', (event) => changeSize('padding', event, padding, paddingLabel, paddingBoxSpan, paddingBox));
+border.addEventListener('input', (event) => changeSize('border', event, border, borderLabel, borderBoxSpan, borderBox));
+margin.addEventListener('input', (event) => changeSize('margin', event, margin, marginLabel, marginBoxSpan, marginBox));
+width.addEventListener('input', (event) => changeSize('width', event, width, widthLabel, null, baseBox));
+height.addEventListener('input', (event) => changeSize('height', event, height, heightLabel, null, baseBox));
 
 
 
@@ -143,18 +120,14 @@ position.addEventListener('change', (e) => {
   positionTarget.style.position = e.target.value;
   demoPositionLabel.textContent = 'position: ' + e.target.value
   switch (e.target.value) {
-    case 'relative': demoPositionLabel.dataset.tooltip = "Относительное позиционирование, элемент не выпадает из потока, за ним сохраняется его место и он позиционируется относительно своего места в потоке (z-index, left и top работают)";
-      break;
-    case 'absolute': demoPositionLabel.dataset.tooltip = "Абсолютное позиционирование, элемент выпадает из потока полностью, другие элементы занимают его место и он позиционируется относительно relative родителя или window (z-index, left и top работают)";
-      break;
-    case 'fixed': demoPositionLabel.dataset.tooltip = "Фиксированное позиционирование, элемент выпадает из потока полностью, другие элементы занимают его место и он позиционируется относительно window, не прокручивается (z-index, left и top работают)";
-      break;
+    case 'relative': demoPositionLabel.dataset.tooltip = "Относительное позиционирование, элемент не выпадает из потока, за ним сохраняется его место и он позиционируется относительно своего места в потоке (z-index, left и top работают)"; break;
+    case 'absolute': demoPositionLabel.dataset.tooltip = "Абсолютное позиционирование, элемент выпадает из потока полностью, другие элементы занимают его место и он позиционируется относительно relative родителя или window (z-index, left и top работают)"; break;
+    case 'fixed': demoPositionLabel.dataset.tooltip = "Фиксированное позиционирование, элемент выпадает из потока полностью, другие элементы занимают его место и он позиционируется относительно window, не прокручивается (z-index, left и top работают)"; break;
     case 'sticky': demoPositionLabel.dataset.tooltip = "Прилипающее позиционирование, элемент не выпадает из потока, за ним сохраняется его место и он позиционируется относительно родителя, не прокручивается пока идет скролл родителя (z-index и top работают) - пример: прилипающие заголовки";
       demoPosition.style.height = '200px';
       demoPosition.style.overflow = 'auto';
       break;
-    default: demoPositionLabel.dataset.tooltip = "Статичное позиционирование, обычное положение элемента в потоке (z-index, left и top не работают)";
-      break;
+    default: demoPositionLabel.dataset.tooltip = "Статичное позиционирование, обычное положение элемента в потоке (z-index, left и top не работают)"; break;
   }
 });
 zIndex.addEventListener('input', (e) => {
@@ -169,3 +142,68 @@ topPos.addEventListener('input', (e) => {
   positionTarget.style.top = e.target.value + 'px';
   demoTopLabel.textContent = 'top: ' + e.target.value
 });
+
+
+// HTML Flex
+const demoFlex = document.querySelector('#demo-flex'),
+      parentModel = document.querySelector('#parentModel'),
+      demoParentModelLabel = document.querySelector('#demo-parentModel-label'),
+      flexDirection = document.querySelector('#flex-direction'),
+      flexDirectionLabel = document.querySelector('#demo-direction-label')
+      flexWrap = document.querySelector('#flex-wrap'),
+      flexWrapLabel = document.querySelector('#demo-wrap-label'),
+      justifyContent = document.querySelector('#justify-content'),
+      demoJustifyLabel = document.querySelector('#demo-justify-label');
+
+parentModel.addEventListener('change', (e) => {
+  demoFlex.style.display = e.target.value;
+  demoParentModelLabel.textContent = 'display: ' + e.target.value;
+  switch (e.target.value) {
+    case 'flex': demoParentModelLabel.dataset.tooltip = 'Flex контейнер, дочерние элементы - блоки'; break;
+    default: demoParentModelLabel.dataset.tooltip = 'Обычный контейнер, дочерние элементы - блоки'; break;
+  }
+})
+flexDirection.addEventListener('change', (e) => {
+  demoFlex.style.flexDirection = e.target.value;
+  flexDirectionLabel.textContent = 'flex-direction: ' + e.target.value;
+  switch (e.target.value) {
+    case 'row-reverse': flexDirectionLabel.dataset.tooltip = 'Ось - X, направление справа - налево'; break;
+    case 'column': flexDirectionLabel.dataset.tooltip = 'Ось - Y, направление сверху - вниз'; break;
+    case 'column-reverse': flexDirectionLabel.dataset.tooltip = 'Ось - Y, направление снизу - вверх'; break;
+    default: flexDirectionLabel.dataset.tooltip = 'Ось - X, направление слева - направо'; break;
+  }
+})
+
+flexWrap.addEventListener('change', (e) => {
+  demoFlex.style.flexWrap = e.target.value;
+  flexWrapLabel.textContent = 'flex-wrap: ' + e.target.value;
+  switch (e.target.value) {
+    case 'wrap': flexWrapLabel.dataset.tooltip = 'Элементы переносятся, насколько требует того их ширина и размер контейнера, порядок сверху - вниз'; break;
+    case 'wrap-reverse': flexWrapLabel.dataset.tooltip = 'Элементы переносятся, насколько требует того их ширина и размер контейнера, порядок снизу - вверх'; break;
+    default: flexWrapLabel.dataset.tooltip = 'Элементы не переносятся (все в одну строку)'; break;
+  }
+})
+
+justifyContent.addEventListener('change', (e) => {
+  demoFlex.style.justifyContent = e.target.value;
+  demoJustifyLabel.textContent = 'justify-content: ' + e.target.value;
+  switch (e.target.value) {
+    case 'flex-end': demoJustifyLabel.dataset.tooltip = 'Выравнивание флекс-элементов с конца'; break;
+    case 'start': demoJustifyLabel.dataset.tooltip = 'Выравнивание элементов в начале'; break;
+    case 'end': demoJustifyLabel.dataset.tooltip = 'Выравнивание элементов в конце'; break;
+    case 'left': demoJustifyLabel.dataset.tooltip = 'Выравнивание элементов по левому краю'; break;
+    case 'right': demoJustifyLabel.dataset.tooltip = 'Выравнивание элементов по правому краю'; break;
+    case 'center': demoJustifyLabel.dataset.tooltip = 'Выравнивание элементов по центру'; break;
+    case 'safe center': demoJustifyLabel.dataset.tooltip = 'Выравнивание элементов по центру, если элемент не помещается в контейнер то он выравнивается по левому краю в обычном порядке'; break;
+    case 'unsafe center': demoJustifyLabel.dataset.tooltip = 'Выравнивание элементов по центру, если элемент не помещается в контейнер то он все равно выравнивается по центру'; break;
+    case 'baseline': demoJustifyLabel.dataset.tooltip = 'Выравнивание относительно осевой линии'; break;
+    case 'first baseline': demoJustifyLabel.dataset.tooltip = 'Выравнивание относительно осевой линии, с flex-start ориентацией'; break;
+    case 'last baseline': demoJustifyLabel.dataset.tooltip = 'Выравнивание относительно осевой линии, с flex-end ориентацией'; break;
+    case 'space-between': demoJustifyLabel.dataset.tooltip = 'Равномерно распределяет все элементы по ширине flex-блока. Первый элемент вначале, последний в конце'; break;
+    case 'space-around': demoJustifyLabel.dataset.tooltip = 'Равномерно распределяет все элементы по ширине flex-блока. Все элементы имеют полноразмерное пространство с обоих концов'; break;
+    case 'space-evenly': demoJustifyLabel.dataset.tooltip = 'Равномерно распределяет все элементы по ширине flex-блока. Все элементы имеют равное пространство вокруг'; break;
+    case 'stretch': demoJustifyLabel.dataset.tooltip = 'Равномерно распределяет все элементы по ширине flex-блока. Все элементы имеют "авто-размер", чтобы соответствовать контейнеру'; break;
+    default: demoJustifyLabel.dataset.tooltip = 'Выравнивание флекс-элементов с начала'; break;
+  }
+})
+
